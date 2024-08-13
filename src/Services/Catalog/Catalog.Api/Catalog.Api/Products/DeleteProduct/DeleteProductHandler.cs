914f31c930e421dd36bac2ce3fbfd12d88e8d745
정성원 <xofldj12345@postmath.co.kr>
2024-08-13 14:07:11 +0900

    Add: Exception Handling

--- src/Services/Catalog/Catalog.Api/Catalog.Api/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Catalog.Api/Products/DeleteProduct/DeleteProductHandler.cs
@@ -4,13 +4,21 @@ namespace Catalog.Api.Products.DeleteProduct
     public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
     public record DeleteProductResult(bool IsSuccess);
 
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required");
+        }
+    }
+
     public class DeleteProductCommandHandler
         (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
         : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
         public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("DeleteProductCommandHandler.Handle {@Command}", command);
+            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
 
             session.Delete<Product>(command.Id);
             await session.SaveChangesAsync(cancellationToken);
