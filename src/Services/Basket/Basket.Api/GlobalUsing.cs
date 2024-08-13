ace8aaa8796a7fcb29e46ca50202559f5af4d91a
정성원 <xofldj12345@postmath.co.kr>
2024-08-13 20:02:12 +0900

    Add: Add Repository And Exceptions in basket

--- src/Services/Basket/Basket.Api/GlobalUsing.cs
+++ src/Services/Basket/Basket.Api/GlobalUsing.cs
@@ -3,5 +3,8 @@ global using BuildingBlocks.CQRS;
 global using Carter;
 global using MediatR;
 global using Mapster;
+global using Marten;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using Basket.Api.Exceptions;
