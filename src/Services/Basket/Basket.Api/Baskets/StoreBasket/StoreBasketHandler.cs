a104a6b25352ad47cf55f8d85c5e7b8c2f3737b6
정성원 <xofldj12345@postmath.co.kr>
2024-08-14 12:15:19 +0900

    Mod: Connect Grpc Coupon and Basket

--- src/Services/Basket/Basket.Api/Baskets/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.Api/Baskets/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.Api.Baskets.StoreBasket
+﻿using Discount.Grpc;
+
+namespace Basket.Api.Baskets.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
@@ -12,11 +14,14 @@
         }
     }
 
-    public class StoreBasketCommandHandler(IBasketRepository repository)
+    public class StoreBasketCommandHandler
+        (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
         : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
+            await DeductDiscount(command.Cart, cancellationToken);
+
             ShoppingCart cart = command.Cart;
 
             //TODO: store basket in database (use Marten - upsert - if exist = update 
@@ -24,6 +29,16 @@
             await repository.StoreBasket(command.Cart, cancellationToken);
 
             return new StoreBasketResult(command.Cart.UserName);
+        }
+
+        private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            // Communicate with Discount.Grpc and calculate lastest prices of products into sc
+            foreach (var item in cart.Items)
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
